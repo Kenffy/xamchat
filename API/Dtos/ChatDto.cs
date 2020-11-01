f690c192dab3292b8e4940180befe5b014cc783c
unknown <voufomarius@yahoo.fr>
2020-11-01 18:02:41 +0100

    End of section 8

--- API/Dtos/ChatDto.cs
+++ API/Dtos/ChatDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Core.Entities;
 
 namespace API.Dtos
@@ -6,10 +7,14 @@ namespace API.Dtos
     public class ChatDto
     {
         public int Id { get; set; }
+        [Required]
         public string Sender { get; set; }
+        [Required]
         public string Receiver { get; set; }
+        [Required]
         public string Message { get; set; }
         public string CreatedOn { get; set; }
+        [Required]
         public string UserId { get; set; }
         public string Username { get; set; }
         public string ImageUrl { get; set; }
